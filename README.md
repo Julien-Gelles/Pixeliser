520b772ad4e21464dc905856cc904006436ddceb
Julien <103409256+Julien-Gelles@users.noreply.github.com>
2023-12-15 00:11:55 +0100

    Pixeliser Project

---
+++ README.md
@@ -0,0 +1,101 @@
+<h1 align="center">
+PIXELISER PROJECT
+</h1>
+
+<p align="center">
+<a href="https://www.python.org">
+<img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white">
+</a>
+</p>
+
+<p align="center">
+<a href="#overview">Overview</a> • <a href="#how-to-use">How To Use</a> • <a href="#color-matching-algorithms">Color Matching Algorithms</a> • <a href="#colors-and-default-array">Colors And Default Array</a> • <a href="#quick-render">Quick Render</a> • <a href="#author">Author</a><br>
+
+<img src="img/OverviewBladerunner.jpg">
+</p>
+
+---
+
+## Overview
+
+This small project pixelizes and recolors an image according to a specified pixel division and color array.<br>
+The project provides a simple yet customizable way to transform images with various options.
+
+## How To Use
+
+### Command Line Options
+
+- `-f` or `--file`: Path to the image file. If not provided, the script launches the file manager for file selection.
+
+- `-p` or `--pixels`: Pixelization degree. Default is set to 16.
+
+- `-c` or `--color_list`: Array of RGB tuples representing colors for recoloring. If not provided, a default color array is used.
+
+- `-v` or `--version`: Recoloring method. Choose between 1 or 2.
+
+- `-q` or `--quick`: Renders a quick version of the image for almost instant display.
+
+- `-i` or `--initial`: Displays both the initial and pixelized images when the code finishes.
+
+### Example Usage
+
+```bash
+$python pixeliser.py -f path/to/image.jpg -p 16 -c "[(255, 0, 0), (0, 255, 0), (0, 0, 255)]" -v 1 -q -i
+```
+
+## Color Matching Algorithms
+
+The script recolors each pixel according to the provided color array and then pixelizes the image based on the specified degree (or pixelises and recolors).
+
+The project uses two algorithms version (`--version`) to match colors :
+
+**1.  CIE Lab Color Space**: A perceptually uniform color space is used for accurate color matching.
+
+**2. Euclidean Distance**: A simple distance calculation in RGB space is employed for an alternative matching approach.
+
+### Here's an example with the two different processes:
+
+<img src="img/versions.jpg">
+
+## Colors And Default Array
+
+If no color array is provided in the options, the default colors are as follows:
+
+|                                                          | RGB                     | HEX         |
+| -------------------------------------------------------- | ----------------------- | ----------- |
+| ![#0e0d0d](https://via.placeholder.com/10/0e0d0d?text=+) | **rgb(14,13,13)**       | **#0e0d0d** |
+| ![#492914](https://via.placeholder.com/10/492914?text=+) | **rgb(73,41,20)**       | **#492914** |
+| ![#7a3f17](https://via.placeholder.com/10/7a3f17?text=+) | **rgb(122,63,23)**      | **#7a3f17** |
+| ![#bc1a1a](https://via.placeholder.com/10/bc1a1a?text=+) | **rgb(188,26,26)**      | **#bc1a1a** |
+| ![#ca541d](https://via.placeholder.com/10/ca541d?text=+) | **rgb(202,84,29)**      | **#ca541d** |
+| ![#ffa928](https://via.placeholder.com/10/ffa928?text=+) | **rgb(255,169,40)**     | **#ffa928** |
+| ![#ffeb35](https://via.placeholder.com/10/ffeb35?text=+) | **rgb(255,235,53)**     | **#ffeb35** |
+| ![#83bc1a](https://via.placeholder.com/10/83bc1a?text=+) | **rgb(131,188,26)**     | **#83bc1a** |
+| ![#1ea711](https://via.placeholder.com/10/1ea711?text=+) | **rgb(30,167,17)**      | **#1ea711** |
+| ![#096800](https://via.placeholder.com/10/096800?text=+) | **rgb(9,104,0)**        | **#096800** |
+| ![#01b185](https://via.placeholder.com/10/01b185?text=+) | **rgb(1,177,133)**      | **#01b185** |
+| ![#78d4ff](https://via.placeholder.com/10/78d4ff?text=+) | **rgb(120,212,255)**    | **#78d4ff** |
+| ![#3861eb](https://via.placeholder.com/10/3861eb?text=+) | **rgb(56,97,235)**      | **#3861eb** |
+| ![#01259c](https://via.placeholder.com/10/01259c?text=+) | **rgb(1,37,156)**       | **#01259c** |
+| ![#20002d](https://via.placeholder.com/10/20002d?text=+) | **rgb(32,0,45)**        | **#20002d** |
+| ![#8e1abc](https://via.placeholder.com/10/8e1abc?text=+) | **rgb(142,26,188)**     | **#8e1abc** |
+| ![#bc1aa6](https://via.placeholder.com/10/bc1aa6?text=+) | **rgb(188,26,166)**     | **#bc1aa6** |
+| ![#ffa8f3](https://via.placeholder.com/10/ffa8f3?text=+) | **rgb(255,168,243)**    | **#ffa8f3** |
+| ![#f6e1cf](https://via.placeholder.com/10/f6e1cf?text=+) | **rgb(246,225,207)**    | **#f6e1cf** |
+| ![#fafafa](https://via.placeholder.com/10/fafafa?text=+) | **rgb(250,250,250)**    | **#fafafa** |
+
+### Here's an example with a grayscale array provided with `--colors`:
+
+<img src="img/greyScale.jpg">
+
+## Quick Render
+
+The `--quick` render option in this script facilitates faster processing by employing a two-step approach: pixelization followed by recoloring (instead of recoloring and then pixelization).
+
+### Here's an example featuring both a classic and a quick render:
+
+<img src="img/quickRender.jpg">
+
+## Author
+
+- [Gelles Julien](https://www.github.com/julien-gelles)
---
+++ pixeliser.py
@@ -0,0 +1,182 @@
+import sys, time, argparse, ast
+from PIL import Image
+from tkinter import filedialog
+
+color_dict = {}
+
+def rgb_to_lab(rgb):
+    r, g, b = rgb
+    r /= 255.0
+    g /= 255.0
+    b /= 255.0
+
+    r = (r / 12.92) if (r <= 0.04045) else ((r + 0.055) / 1.055) ** 2.4
+    g = (g / 12.92) if (g <= 0.04045) else ((g + 0.055) / 1.055) ** 2.4
+    b = (b / 12.92) if (b <= 0.04045) else ((b + 0.055) / 1.055) ** 2.4
+
+    r *= 100.0
+    g *= 100.0
+    b *= 100.0
+
+    x = r * 0.4124564 + g * 0.3575761 + b * 0.1804375
+    y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750
+    z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041
+
+    x /= 95.047
+    y /= 100.000
+    z /= 108.883
+
+    x = (x ** (1.0 / 3.0)) if (x > 0.008856) else (903.3 * x + 16.0) / 116.0
+    y = (y ** (1.0 / 3.0)) if (y > 0.008856) else (903.3 * y + 16.0) / 116.0
+    z = (z ** (1.0 / 3.0)) if (z > 0.008856) else (903.3 * z + 16.0) / 116.0
+
+    l = max(0.0, 116.0 * y - 16.0)
+    a = (x - y) * 500.0
+    b = (y - z) * 200.0
+
+    return l, a, b
+
+def color_distance(color1, color2):
+    l1, a1, b1 = color1
+    l2, a2, b2 = color2
+    delta_l = l2 - l1
+    delta_a = a2 - a1
+    delta_b = b2 - b1
+
+    distance = (delta_l ** 2 + delta_a ** 2 + delta_b ** 2) ** 0.5
+    return distance
+
+
+def getCloserColor(target_color, color_list):
+    lab_target_color = rgb_to_lab(target_color)
+    lab_color_list = [rgb_to_lab(color) for color in color_list]
+
+    min_distance = float('inf')
+    closest_color_index = -1
+
+    for i, lab_color in enumerate(lab_color_list):
+        distance = color_distance(lab_target_color, lab_color)
+        if distance < min_distance:
+            min_distance = distance
+            closest_color_index = i
+
+    return color_list[closest_color_index]
+
+#------------------------------------------------
+
+def getCloserColor2(rgb_color, color_list):
+    min_distance = float('inf')
+    closest_color = None
+
+    for color in color_list:
+        distance = sum((a - b) ** 2 for a, b in zip(rgb_color, color))
+        if distance < min_distance:
+            min_distance = distance
+            closest_color = color
+
+    return closest_color
+
+#------------------------------------------------
+
+def recolorImage(image, color_list, version):
+    new_im = Image.new("RGB", image.size)
+    for x in range(image.width):
+        for y in range(image.height):
+            pixel_values = image.getpixel((x, y))
+            (r, g, b) = pixel_values[:3]
+            if ((r,g,b) in color_dict):
+                color = color_dict[(r,g,b)]
+            else:
+                match version:
+                    case 1:
+                        color = getCloserColor((r, g, b), color_list)
+                    case 2:
+                        color = getCloserColor2((r, g, b), color_list)
+                color_dict[(r,g,b)] = color
+            new_im.putpixel((x,y), color)
+    return new_im
+
+def pixeliseImage(pixelDegree, image, color_list, version, quick):
+    if quick:
+        small_im = image.resize((int(image.width/pixelDegree),int(image.height/pixelDegree)), resample=Image.Resampling.BILINEAR)
+        return recolorImage(small_im, color_list, version).resize(image.size, Image.Resampling.NEAREST)
+    else:
+        recolor_im = recolorImage(image, color_list, version)
+        small_im = recolor_im.resize((int(image.width/pixelDegree),int(image.height/pixelDegree)), resample=Image.Resampling.BILINEAR)
+        return small_im.resize(image.size, Image.Resampling.NEAREST)
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file')
+    parser.add_argument('-p', '--pixels')
+    parser.add_argument('-c', '--color_list')
+    parser.add_argument('-v', '--version')
+    parser.add_argument('-q', '--quick', action='store_true')
+    parser.add_argument('-i', '--initial', action='store_true')
+    args = parser.parse_args()
+
+    file_arg = args.file
+    pixels_arg = args.pixels
+    color_list_arg = args.color_list
+    version_arg = args.version
+    quick_arg = args.quick
+    initial_args = args.initial
+
+    if file_arg == None:
+        file_types = [("Image files", "*.png"),("Image files", "*.jpg"),("Image files", "*.jpeg"),("Image files", "*.bmp"),("Image files", "*.ppm"),("Image files", "*.pgm")]
+        file = filedialog.askopenfilename(filetypes=file_types)
+        if (file == None or file == ()):
+            sys.exit()
+        file_arg = file
+    if pixels_arg == None:
+        pixels_arg = 16
+    if color_list_arg == None:
+        color_list_arg = [
+            (73,41,20),
+            (122,63,23),
+            (188,26,26),
+            (202,84,29),
+            (255,169,40),
+            (255,235,53),
+            (131,188,26),
+            (30,167,17),
+            (9,104,0),
+            (1,177,133),
+            (120,212,255),
+            (56,97,235),
+            (1,37,156),
+            (32,0,45),
+            (142,26,188),
+            (188,26,166),
+            (255,168,243),
+            (246,225,207),
+            (250,250,250),
+            (14,13,13)
+        ]
+    else:
+        color_list_arg = ast.literal_eval(color_list_arg)
+    if version_arg == None:
+        version_arg = 1
+
+    try:
+        print("Program runnin..")
+        start_time = time.time()
+        im = Image.open(file_arg)
+        final_im = pixeliseImage(int(pixels_arg), im, color_list_arg, int(version_arg), quick_arg)
+        if initial_args:
+            im.show()
+        final_im.show()
+        end_time = time.time()
+        duration = round(end_time - start_time,2)
+        if duration > 1: print("Done in", round(end_time - start_time,2) , "seconds.")
+        else: print("Done in", round(end_time - start_time,2) , "second.")
+    except:
+        print("Bad Input :c") 
